da62530bf48adef29cb184ec9af6b07bc02bd973
Peter Piech <pdpiech@gmail.com>
2016-04-25 16:59:14 -0400

    Create package structure

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+if __name__ == "__main__":
+    setup(name="prefpy-experiments",
+          version="0.0.1",
+          description="Rank aggregation algorithms",
+          classifiers=[
+              "Development Status :: 3 - Alpha",
+              "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+              "Programming Language :: Python :: 3.4",
+              "Programming Language :: Python :: 3.5",
+              "Intended Audience :: Science/Research",
+              "Topic :: Scientific/Engineering"
+          ],
+          url="http://github.com/pdpiech/prefpy-experiments",
+          author="Peter Piech",
+          license="GPL-3",
+          packages=["prefpy-experiments"],
+          install_requires=["prefpy"],
+          zip_safe=False)
